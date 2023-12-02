321ade59fca15a591e482cb1c7194bf1c93c6b19
Bastian Block <git@bblock.dev>
2023-12-02 16:09:18 +0100

    [WIP] Day02

---
+++ src/day02/solution.kt
@@ -0,0 +1,20 @@
+package day01
+
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        var sum = 0
+        return sum
+    }
+
+    fun part2(input: List<String>): Int {
+        var sum = 0
+        return sum
+    }
+
+    val input = readInput("day02")
+    part1(input).println()
+    part2(input).println()
+}
