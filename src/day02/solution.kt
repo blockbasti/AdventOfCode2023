9d36fe6398e850ffbf73c6e4d1f2728db19c8d71
Bastian Block <git@bblock.dev>
2023-12-03 13:00:22 +0100

    day02 part2

--- src/day02/solution.kt
+++ src/day02/solution.kt
@@ -1,16 +1,75 @@
-package day01
+package day02
 
 import println
 import readInput
+import kotlin.math.max
 
 fun main() {
     fun part1(input: List<String>): Int {
+        val RED = 12
+        val GREEN = 13
+        val BLUE = 14
+
         var sum = 0
+
+        input.forEach { line ->
+            val gameId = Regex("Game (\\d+):").find(line)!!.groupValues[1].toInt()
+
+            val sets = line.removePrefix("Game $gameId:").split(";", ",")
+            var wasOver = false
+            sets.forEach { set ->
+                val count = Regex("\\d+").find(set)!!.value.toInt()
+                when {
+                    set.contains("red") -> {
+                        if (count > RED) wasOver = true
+                    }
+
+                    set.contains("green") -> {
+                        if (count > GREEN) wasOver = true
+                    }
+
+                    set.contains("blue") -> {
+                        if (count > BLUE) wasOver = true
+                    }
+                }
+            }
+            if (!wasOver) {
+                sum += gameId
+            }
+        }
+
         return sum
     }
 
     fun part2(input: List<String>): Int {
         var sum = 0
+
+        input.forEach { line ->
+            val gameId = Regex("Game (\\d+):").find(line)!!.groupValues[1].toInt()
+
+            val sets = line.removePrefix("Game $gameId:").split(";", ",")
+            var red = 0
+            var blue = 0
+            var green = 0
+            sets.forEach { set ->
+                val count = Regex("\\d+").find(set)!!.value.toInt()
+                when {
+                    set.contains("red") -> {
+                        red = max(count, red)
+                    }
+
+                    set.contains("green") -> {
+                        green = max(count, green)
+                    }
+
+                    set.contains("blue") -> {
+                        blue = max(count, blue)
+                    }
+                }
+            }
+            sum += red * green * blue
+        }
+
         return sum
     }
 
