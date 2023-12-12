098b4d34df8c2d9f05cd0194c717a77516959170
Bastian Block <git@bblock.dev>
2023-12-12 23:27:59 +0100

    day12 part2 broken

--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,22 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+// https://www.reddit.com/r/Kotlin/comments/isg16h/comment/g5fvsw3/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+fun <T> Iterable<T>.combinations(length: Int): Sequence<List<T>> =
+    sequence {
+        val pool = this@combinations as? List<T> ?: toList()
+        val n = pool.size
+        if (length > n) return@sequence
+        val indices = IntArray(length) { it }
+        while (true) {
+            yield(indices.map { pool[it] })
+            var i = length
+            do {
+                i--
+                if (i == -1) return@sequence
+            } while (indices[i] == i + n - length)
+            indices[i]++
+            for (j in i + 1 until length) indices[j] = indices[j - 1] + 1
+        }
+    }
--- src/day12/solution.kt
+++ src/day12/solution.kt
@@ -1,46 +1,52 @@
 package day12
 
+import combinations
 import day12.Record
 import println
 import readInput
-import java.awt.SystemColor.window
 import kotlin.time.measureTime
+
 
 private typealias Record = Pair<String, List<Int>>
 
 fun main() {
+    val cache = HashMap<Record, Long>()
 
     fun satisfiesCounts(record: Record): Boolean {
+        //if (cacheBool.get(record) != null) return cacheBool.get(record)!!
         val (springs, counts) = record
         val groups = springs
             .split(".")
             .filter { it.isNotBlank() }
             .map { it.length }
 
-        if (groups.size != counts.size)
+        if (groups.size != counts.size) {
             return false
+        }
 
-        return groups
+        val result = groups
             .zip(counts)
             .all { it.first == it.second }
+
+        return result
     }
 
     fun couldSatisfyCount(springs: String, counts: List<Int>): Boolean {
-        val groups = springs
+        val result = springs
             .split(".")
             .filter { it.isNotBlank() }
             .takeWhile { !it.contains('?') }
             .map { it.length }
-
-        return groups
             .zip(counts)
             .all { it.first == it.second }
+
+        return result
     }
 
     fun canInsertGroup(old: String, new: String): Boolean {
         return old.zip(new).all {
             when (it.first) {
-                '?' -> it.second == '#' || it.second =='.'
+                '?' -> it.second == '#' || it.second == '.'
                 '#' -> it.second == '#'
                 '.' -> it.second == '.'
                 else -> false
@@ -49,18 +55,75 @@ fun main() {
 
     }
 
-    fun generatePermutationsOptimized(record:Record): List<String>{
+    fun generatePermutationsOptimized(record: Record): List<String> {
         val (springs, counts) = record
 
-        val springGroups = counts.map { "#".repeat(it).plus('.') }
+        val springGroups = counts.map { "#".repeat(it) }
 
-        val result = springs.windowed(springGroups.first().length+1).filter { window -> canInsertGroup(window, springGroups.first().plus(window.substring(springGroups.first().length))) }
+        val opts =
+            (0..(counts.size + (springs.length - counts.joinToString("").length - 1))).combinations(springGroups.size)
 
-        return if (springs.contains('?')) {
-            val first = generatePermutationsOptimized(springs.replaceFirst('?', '.') to counts)
-            val second = generatePermutationsOptimized(springs.replaceFirst('?', '#') to counts)
-            first.plus(second)
-        } else listOf(springs)
+        val new = opts.map { opt ->
+            var newSpring = springs
+            opt.forEachIndexed { index, i ->
+                newSpring = newSpring.replaceRange(
+                    i,
+                    (i + springGroups[index].length).coerceAtMost(springs.length),
+                    springGroups[index]
+                )
+            }
+            return@map newSpring.substring(springs.indices)
+        }
+            .map { it.replace('?', '.') }
+            //.also { it.toList().println() }
+            .filter { canInsertGroup(springs, it) && satisfiesCounts(it to record.second) }
+            .distinct()
+            .toList()
+
+        return new
+    }
+
+    fun countPermutations(record: Record): Long {
+        val (springs, counts) = record
+        /*if (cache.contains(record)) {
+            return cache[record]!!
+        }*/
+
+        if (springs.isBlank()) {
+            return if (counts.isEmpty()) 1 else 0
+        }
+
+        val permutations: Long
+
+        when (springs.first()) {
+            '.' -> permutations = countPermutations(springs.substring(1) to counts)
+            '?' -> permutations = countPermutations(".".plus(springs.substring(1)) to counts) +
+                    countPermutations("#".plus(springs.substring(1)) to counts)
+
+            else -> {
+                if (counts.isEmpty()) permutations = 0
+                else {
+                    val damaged = counts.first()
+                    if (damaged <= springs.length &&
+                        springs.chars().limit(damaged.toLong()).allMatch { it.toChar() == '#' || it.toChar() == '?' }
+                    ) {
+                        val newCounts = counts.subList(1, counts.size)
+                        permutations = when {
+                            damaged == springs.length -> if (newCounts.isEmpty()) 1 else 0
+                            springs[damaged] == '#' -> countPermutations(springs.substring(damaged + 1) to newCounts)
+                            springs[damaged] == '?' -> countPermutations(".".plus(springs.substring(damaged + 1)) to newCounts)
+                            else -> 0
+                        }
+                    } else {
+                        permutations = 0
+                    }
+                }
+            }
+
+        }
+        cache[record] = permutations
+
+        return permutations
     }
 
     fun generatePermutations(record: Record): List<String> {
@@ -73,14 +136,72 @@ fun main() {
         } else listOf(springs)
     }
 
-    fun countValidPermutations(record: Record): Int {
+    fun countValidPermutations(record: Record): Long {
         val (springs, counts) = record
+        if (cache.containsKey(record)) {
+            return cache.get(record)!!
+        }
 
-        return generatePermutationsOptimized(record)
-            //.also { it.println() }
-            .filter { !it.contains('?') && satisfiesCounts(it to counts) }
-            .size
+        if (springs.isBlank()) {
+            // We have processed the complete condition, so if there are no groups left we
+            // have a valid permutation, otherwise a non-valid one.
+            return if (springs.isEmpty()) 1 else 0
+        }
+
+        val firstChar: Char = springs.get(0)
+        var permutations: Long = 0
+        if (firstChar == '.') {
+            // Working spring, so just skip over it and continue.
+            permutations = countValidPermutations(springs.substring(1) to counts)
+        } else if (firstChar == '?') {
+            // Spring of unknown status, so count the permutations for both a working and a
+            // damaged spring.
+            permutations = (countPermutations("." + springs.substring(1) to counts)
+                    + countPermutations("#" + springs.substring(1) to counts))
+        } else {
+            // First character is #
+            if (counts.isEmpty()) {
+                // No more groups but we still have a #, this is not valid.
+                permutations = 0
+            } else {
+                val nrDamaged: Int = counts.get(0)
+                // We want to include a group of nrDamaged springs, so the remaining length of
+                // the condition should be at least the size of that group. Also, the next
+                // nrDamaged springs should all be damaged (#) or unknown (?).
+                if (nrDamaged <= springs.length
+                    && springs.chars().limit(nrDamaged.toLong())
+                        .allMatch { c -> c.toChar() === '#' || c.toChar() === '?' }
+                ) {
+                    val newGroups: List<Int> = counts.subList(1, counts.size)
+                    permutations = if (nrDamaged == springs.length) {
+                        // The remaining length of the condition is equal to the nr of damaged springs
+                        // in the group, so we are done if there are no remaining groups.
+                        (if (newGroups.isEmpty()) 1 else 0).toLong()
+                    } else if (springs.get(nrDamaged) === '.') {
+                        // We have just added a group of damaged springs (#) and the next spring is
+                        // operational, this is valid, so skip over that operational spring.
+                        countValidPermutations(springs.substring(nrDamaged + 1) to newGroups)
+                    } else if (springs.get(nrDamaged) === '?') {
+                        // We have just added a group of damaged springs (#), so the next spring can
+                        // only be operational (.).
+                        countValidPermutations("." + springs.substring(nrDamaged + 1) to newGroups)
+                    } else {
+                        // We have just added a group of damaged springs (#), but the next character is
+                        // also a damaged spring, this is not valid.
+                        0
+                    }
+                } else {
+                    // Either size of the group is greater than the remaining length of the
+                    // condition or the next nrDamaged springs are not either damaged (#) or unknown
+                    // (?).
+                    permutations = 0
+                }
+            }
+        }
+        cache.put(record, permutations)
+        return permutations
     }
+
 
     fun part1(input: List<String>): Long {
         val records: List<Record> = input.map { line ->
@@ -89,7 +210,7 @@ fun main() {
         }
 
         return records.sumOf { record ->
-            countValidPermutations(record).toLong()
+            countValidPermutations(record)
         }
     }
 
@@ -104,8 +225,8 @@ fun main() {
         var index = 0
         measureTime {
             sum = records.sumOf { record ->
-                println(index++)
-                countValidPermutations(record).toLong()
+                index++.println()
+                countValidPermutations(record)
             }
         }.println()
 
